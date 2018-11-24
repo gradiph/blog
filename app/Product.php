a7eba0bf72e015b82cd811788432317316047d67
Gradi <gradiph@gmail.com>
2018-11-24 22:42:39 +0700

    Product model

---
+++ app/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'view_count',
+    ];
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'name' => 'string',//string
+        'description' => 'string',//text
+        'price' => 'integer',//integer
+        'view_count' => 'integer',//integer
+    ];
+
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    /**
+     * Accessor
+     */
+
+    /**
+     * Mutator
+     */
+
+    /**
+     * Relationship
+     */
+}
