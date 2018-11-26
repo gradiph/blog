c7fd53719ddab5347448959dea6bec04e30ffbe9
Gradi <gradiph@gmail.com>
2018-11-26 18:24:58 +0700

    Admin seeder

--- app/Admin.php
+++ app/Admin.php
@@ -11,21 +11,30 @@ class Admin extends Authenticatable
 
     protected $guard = 'admin';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    protected $casts = [
+        'name' => 'string',//string
+        'email' => 'string',//email
+    ];
+
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Accessor
+     */
+
+    /**
+     * Mutator
+     */
+
+    /**
+     * Relationship
+     */
 }
